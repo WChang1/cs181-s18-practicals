d9208bac1d6c6c20d67bcd829dc626e835ae5e12
Peter Tian <ptian@Peters-MacBook-Pro-4.local>
2018-04-04 03:58:12 -0400

    python notebook

--- P3/p3_starter.py
+++ P3/p3_starter.py
@@ -12,16 +12,18 @@ reader = csv.reader(f)
 # Right now it is taking rows 0 to 70
 # and columns 0:10
 lst = []
-for row in itertools.islice(reader, 0, 70):
-    lst.append(map(float, row[0:10]))
+for row in itertools.islice(reader, 0, 2):
+    lst.append(map(float, row[0:88201]))
 
 # Turn into array
 array = np.array(lst)
-print(array.shape)
+print array[:,-1]
 
 # Apply PCA
 #
 # Documentation: 
 # http://scikit-learn.org/stable/modules/generated/sklearn.decomposition.PCA.html
-pca = PCA(n_components=3)
+pca = PCA(n_components=100)
 pca_vectors_array = pca.fit_transform(array)
+
+print pca_vectors_array.shape
